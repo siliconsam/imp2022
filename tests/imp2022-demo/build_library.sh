ef8f8dde1fb4638ffe9085a0e590cc020ec214b7
siliconsam <jdmcmullin@aol.com>
2023-09-11 15:25:01 +0100

    Amended demo code to show similarity between bilbo.imp and bilbo.c Also corrected error in build_library.sh script

--- tests/imp2022-demo/build_library.sh
+++ tests/imp2022-demo/build_library.sh
@@ -7,15 +7,3 @@ gcc -m32 -shared -z notext -fPIC -Wl,-soname,libbilbo.so -o libbilbo.so bilbo.o
 # Move the shareable library to make it visible when linking
 #
 cp libbilbo.so /usr/local/lib
-#
-# Now, compile a program to utilise the routines in the bilbo.s assembler file
-#
-imp77 -c -Fc -Fs -Fi baggins.imp
-#
-# Link the baggins code with the shareable library
-#
-gcc -m32 -no-pie -o baggins baggins.o -lbilbo -limp77 -lm
-#
-# Run the new program
-#
-./baggins
